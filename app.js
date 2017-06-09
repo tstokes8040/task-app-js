d5c8225221e32a70c636eb6855deb661b70bac1c
Tyler Stokes <tylerstokes8040@gmail.com>
2017-06-08 20:08:34 -0500

    Application JavaScript

--- app.js
+++ app.js
@@ -2,20 +2,38 @@ window.addEventListener('load', function() {
   const addButton = document.querySelector('.add-task-button');
   const taskList = document.querySelector('.task-list');
 
-  addButton.addEventListener('click', function(){
+  //Add new task with user clicks add button
+  addButton.addEventListener('click', function (){
+    addTask();
+  });
+
+  //Add a task when enter key is pressed
+  document.addEventListener('keydown', function (event) {
+    const keyName = event.key;
+    if (keyName == 'Enter' && document.querySelector('.task-name input').value != "") {
+      addTask();
+    }
+  });
+
+  //Task class
+  //COMING SOON
+
+  //Add a new task to local storage
+  function addTask() {
     let taskName = document.querySelector('.task-name input').value;
     localStorage.setItem(taskName, 'false');
     taskList.insertAdjacentHTML('beforeend','<p class="task">' + taskName + '</p>');
     document.querySelector('.task-name input').value = "";
-  });
+  }
 
   //Get all tasks from local storage
   function getAllTasks () {
-    for (let i = 0; i < localStorage.length; i++) {
+    for (let i = localStorage.length - 1 ; i >= 0; i--) {
       let taskName = localStorage.key(i);
       taskList.insertAdjacentHTML(`beforeend`, `<p class="task">${taskName}</p>`);
     }
   }
 
   getAllTasks();
+
 });
